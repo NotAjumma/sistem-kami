47137b44f725c398b22e7e5853c92dbc293a7d53
Akmal <>
2025-07-13 02:09:06 +0800

    add google analytics user visit

--- resources/views/home/homeLayout.blade.php
+++ resources/views/home/homeLayout.blade.php
@@ -8,6 +8,15 @@
 <html lang="en">
 
 <head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WF0W2KJX24"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-WF0W2KJX24');
+	</script>
 	<!--Title-->
 	<title>
 		@if (request()->is('/'))
