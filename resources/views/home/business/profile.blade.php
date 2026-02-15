bf05f9f027e6f1f986842259af23104600babc6d
akmal <akmaladha@MacBook-Pro-Akmal.local>
2026-02-15 20:29:27 +0800

    add pax to slot

--- resources/views/home/business/profile.blade.php
+++ resources/views/home/business/profile.blade.php
@@ -916,9 +916,9 @@
                             <div class="portfolio-content">
                                 <h4 class="event-title portfolio-title text-center">{{ $slot->slot_name ?? 'Tema' }}</h4>
                                 @if($organizer->what_flow == 2)
-                                <h4 class="event-title portfolio-title text-center">RM {{ $slot->slot_price }}</h4>
-                                <h4 class="event-title portfolio-title text-center">{{ $slot->duration_minutes }} minit</h4>
-
+                                    <h4 class="event-title portfolio-title text-center">RM {{ $slot->slot_price }}</h4>
+                                    <h4 class="event-title portfolio-title text-center">{{ $slot->duration_minutes }} minit</h4>
+                                    <h4 class="event-title portfolio-title text-center">{{ $slot->pax }}</h4>
                                 @endif
                                 <div class="mt-3">
                                     <button type="button"
